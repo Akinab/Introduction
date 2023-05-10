4071f575ce50ed590fd3518b99f87092bf3b9062
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-10 22:48:21 +0800

    Defining Introduction

--- intro.py
+++ intro.py
@@ -4,3 +4,6 @@ def details ():
     print ("\nDescription: {}\nDate: {}".format (Description , Date))
     
 details ()
+
+Intro = input("Hi, my name is Vanica Ocampo Lobusta.\nI want to be a billionaire, so fr**king bad. Buy all of the things I never had.\nI wanna on the over of Forbes magazine, smiling next to Oprah and the Queen.")
+
