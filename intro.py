4be9323b5d14216bb12fc0e1e45fe93d1eff9fa9
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-10 22:51:02 +0800

    printing output

--- intro.py
+++ intro.py
@@ -7,3 +7,4 @@ details ()
 
 Intro = input("Hi, my name is Vanica Ocampo Lobusta.\nI want to be a billionaire, so fr**king bad. Buy all of the things I never had.\nI wanna on the over of Forbes magazine, smiling next to Oprah and the Queen.")
 
+print(Intro)
